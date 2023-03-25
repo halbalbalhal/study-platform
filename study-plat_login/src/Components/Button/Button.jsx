ba744ee913f381c01853af29fc07a2091f77cd9b
Makar <makar.ryzhko00@mail.ru>
2023-03-25 13:50:23 +0300

    Update Button.jsx

--- study-plat_login/src/Components/Button/Button.jsx
+++ study-plat_login/src/Components/Button/Button.jsx
@@ -4,9 +4,6 @@ function Button(props) {
     return(
         <button className='column__loginBtn'>{props.title}</button>
     )
-    function Width (className) {
-        document.querySelector(className).style.width = '20px'
-    }
 }
 
 export default Button
