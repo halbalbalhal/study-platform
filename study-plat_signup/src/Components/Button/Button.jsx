42cbf5db3ad8c5451efa917fc97691d64b6e9779
Makar <makar.ryzhko00@mail.ru>
2023-03-25 14:17:15 +0300

    Fixed css

--- study-plat_signup/src/Components/Button/Button.jsx
+++ study-plat_signup/src/Components/Button/Button.jsx
@@ -4,9 +4,6 @@ function Button(props) {
     return(
         <button className='column__loginBtn'>{props.title}</button>
     )
-    function Width () {
-        document.querySelector('.column__loginBtn').style.width = '20px'
-    }
 }
 
 export default Button
