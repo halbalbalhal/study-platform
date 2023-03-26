d4fe9c0b728790d1a4a8114b28904d9f0e6ac594
halbalbalhal <sanyabirulya@mail.ru>
2023-03-26 18:40:36 +0300

    добавил анимацию появления

--- study-plat/src/containers/onboardPage/onboard.jsx
+++ study-plat/src/containers/onboardPage/onboard.jsx
@@ -11,18 +11,18 @@ const Onboard = () => {
     function Click1() {
         document.getElementById("first_slide").style.display = "none"
         render (
-            <section className='slide' id='second_slide'>
+            <section className='onboard_slide' id='second_slide'>
                 <img src={require("../../assets/images/Onboard/Board2.png")} alt="sdasdasasddas" />
-                <span className='title'>Learn your passion, level up your skill</span>
-                <p className='text'>make yourself expert your skill by studying from mentors</p>
+                <span className='onboard_title'>Learn your passion, level up your skill</span>
+                <p className='onboard_text'>make yourself expert your skill by studying from mentors</p>
 
-                <div className='circles'>
+                <div className='onboard_circles'>
                     <img src={require("../../assets/icons/Onboard/whiteCircle.png")} alt="sdasdasasddas" />
                     <img src={require("../../assets/icons/Onboard/blackCircle.png")} alt="sdasdasasddas" />
                     <img src={require("../../assets/icons/Onboard/whiteCircle.png")} alt="sdasdasasddas" />
                 </div>
 
-                <button className='button' onClick={Click2}>Continue</button>
+                <button className='onboard_button' onClick={Click2}>Continue</button>
             </section>
         )
     }
@@ -31,18 +31,18 @@ const Onboard = () => {
     function Click2() {
         document.getElementById("second_slide").style.display = "none"
         render (
-            <section className='slide'>
+            <section className='onboard_slide'>
                 <img src={require("../../assets/images/Onboard/Board3.png")} alt="sdasdasasddas" />
-                <span className='title'>Get your graduate with extraordinary skills</span>
-                <p className='text'>get your certificate after finished your online course</p>
+                <span className='onboard_title'>Get your graduate with extraordinary skills</span>
+                <p className='onboard_text'>get your certificate after finished your online course</p>
 
-                <div className='circles'>
+                <div className='onboard_circles'>
                     <img src={require("../../assets/icons/Onboard/whiteCircle.png")} alt="sdasdasasddas" />
                     <img src={require("../../assets/icons/Onboard/whiteCircle.png")} alt="sdasdasasddas" />
                     <img src={require("../../assets/icons/Onboard/blackCircle.png")} alt="sdasdasasddas" />
                 </div>
 
-                <button className='button'>Continue</button>
+                <button className='onboard_button'>Continue</button>
             </section>
         )
     }
@@ -50,18 +50,18 @@ const Onboard = () => {
 
     // Rendering first block
     return(
-        <section className='slide' id='first_slide'>
+        <section className='onboard_slide' id='first_slide'>
             <img src={require("../../assets/images/Onboard/Board1.png")} alt="sdasdasasddas" />
-            <span className='title'>Start your journey with fun learning videos</span>
-            <p className='text'>explore various learning videos based on your passion</p>
+            <span className='onboard_title'>Start your journey with fun learning videos</span>
+            <p className='onboard_text'>explore various learning videos based on your passion</p>
 
-            <div className='circles'>
+            <div className='onboard_circles'>
                 <img src={require("../../assets/icons/Onboard/blackCircle.png")} alt="sdasdasasddas" />
                 <img src={require("../../assets/icons/Onboard/whiteCircle.png")} alt="sdasdasasddas" />
                 <img src={require("../../assets/icons/Onboard/whiteCircle.png")} alt="sdasdasasddas" />
             </div>
 
-            <button className='button' onClick={Click1}>Continue</button>
+            <button className='onboard_button' onClick={Click1}>Continue</button>
         </section>
     )
 
