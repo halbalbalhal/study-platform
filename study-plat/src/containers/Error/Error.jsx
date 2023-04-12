bb96640ad77f65239cfade6a6baa9ae250bc1ddb
Makar <makar.ryzhko00@mail.ru>
2023-04-12 20:15:27 +0300

    Fixed bugs

--- study-plat/src/containers/Error/Error.jsx
+++ study-plat/src/containers/Error/Error.jsx
@@ -1,12 +1,12 @@
 import styles from './Error.module.css'
 import Button from '../../components/Button/Button'
 
-const Error = () => {
+const Error = (props) => {
     return(
         <div className={styles.error}>
-            <span className={styles.error__value}>500</span>
-            <span className={styles.error__text}>Server error</span>
-            <p className={styles.error__description}>There was an error on the server. Please wait, we will fix it soon.</p>
+            <span className={styles.error__value}>{props.value}</span>
+            <span className={styles.error__text}>{props.text}</span>
+            <p className={styles.error__description}>{props.description}</p>
             <div className={styles.error__border}></div>
             <div className={styles.error__btn}>
                 <Button title='Go to home'/>
--- study-plat/src/routes/routesConfig.js
+++ study-plat/src/routes/routesConfig.js
@@ -16,7 +16,7 @@ const routesConfig = [
     {
         path: '/error',
         exact: true,
-        element: <Error />
+        element: <Error value='500' text='Server error' description='There was an error on the server. Please wait, we will fix it soon.' />
     }
 ]
 
