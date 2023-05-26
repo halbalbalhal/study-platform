d3e28cde7c71100bce7dbb82417a0d94d322d411
halbalbalhal <sanyabirulya@mail.ru>
2023-05-26 11:19:49 +0300

    made berry as a prop

---
+++ study-plat/src/components/Berry/Berry.jsx
@@ -0,0 +1,10 @@
+import styles from './Berry.module.css'
+
+const Berry = (props) => {
+    return(
+        <div className={styles.homescreen__image}></div>
+    )
+
+}
+
+export default Berry
--- study-plat/src/components/Header/Home/Home.jsx
+++ study-plat/src/components/Header/Home/Home.jsx
@@ -1,17 +1,19 @@
 import styles from './Home.module.css'
 import Button from '../../Button'
-import Berry from '../../../assets/images/Header/blackberry.png'
+
+import Berry from '../../Berry/Berry'
+
 
 const Home = () => {
     return (
         <section className={styles.homescreen}>
             <div className={styles.homescreen__wrapper}>
                 <div className={styles.homescreen__info}>
-                    <span className={styles.homescreen__title}>Explore new <span className={styles.homescreen__blue}>skills</span> with <span className={styles.homescreen__green}>School</span></span>
+                    <span className={styles.homescreen__title}>Explore new <span className={styles.homescreen__blue}>skills</span> <span className={styles.homescreen__purple}>with</span> <span className={styles.homescreen__green}>School</span></span>
                     <p className={styles.homescreen__text}>choose a new <span className={styles.homescreen__red}>profession</span>, that can change the future of the whole <span className={styles.homescreen__yellow}>world</span></p>
                     <Button className={styles.homescreen__button} title='Go study' />
                 </div>
-                <img src={Berry} className={styles.homescreen__image} />                
+                <Berry />
             </div>
         </section>
     )
