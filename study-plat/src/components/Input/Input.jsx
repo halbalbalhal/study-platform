47f9d38b333c109e50cc9ebca1a3b6f4f5bd27cf
Kirill Maximchik <78115664+Flut9@users.noreply.github.com>
2023-05-27 15:39:55 +0300

    Auth

--- study-plat/src/components/Input/Input.jsx
+++ study-plat/src/components/Input/Input.jsx
@@ -1,10 +1,37 @@
 import styles from './Input.module.css'
 
-const Input = (props) => {
+const getData = (inputName) => {
+    switch (inputName) {
+        case 'name':
+            return {
+                errorMessage: 'Имя должно содержать только буквы и состоять из двух слов',
+                regex: /[a-zA-Z]+$/
+            }
+        case 'email':
+            return {
+                errorMessage: 'Почта должна содержать "@" и "."',
+                regex: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+            }
+        case 'password':
+            return {
+                errorMessage: 'Пароль должен содержать 1 заглавную букву, 1 маленькую, цифру и быть не короче 6 символов',
+                regex: /((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]))(?=.{6,})/
+            }
+        default: return { errorMessage: '' }
+    }
+}
+
+const Input = ({ name, register, title, type, isValid }) => {
+    const data = getData(name)
+
     return(
         <div className={styles.column}>
-            <input type={props.type} className={styles.column__input} required />
-            <label for='' className={styles.column__labelForInput}>{props.title}</label>
+            <input
+                type={type}
+                {...register(name, { required: true, pattern: data.regex })}
+                className={`${styles.column__input} ${!isValid && styles.error}`}
+            />
+            <label for='' className={styles.column__labelForInput}>{title}</label>
         </div>
     )
 }
--- study-plat/src/containers/Contacts/Contact.jsx
+++ study-plat/src/containers/Contacts/Contact.jsx
@@ -1,27 +1,8 @@
 import styles from './Contacts.module.css'
 
 import ContactsInfo from '../../components/ContactsInfo/ContactsInfo'
-import { getCourse, getCourses } from '../../firebase/coursesService'
-import { getTasks, getTask } from '../../firebase/tasksService'
 
 const Contact = () => {
-    getTask(
-        "web",
-        "first_lesson",
-        "4smb75Orsex2BFif3MqZ",
-        (data) => {
-            console.log(data)
-        }
-    )
-
-    getTasks(
-        "web",
-        "first_lesson",
-        (data) => {
-            console.log(data)
-        }
-    )
-
     return (
         <>
             <ContactsInfo />
