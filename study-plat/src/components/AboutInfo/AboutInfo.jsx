cdf7f8ce2602f48c8620fa1d59647f725fc1b786
Stupid_Lumpen <20miroslav06@gmail.com>
2023-06-09 13:04:09 +0300

    Доделал About

--- study-plat/src/components/AboutInfo/AboutInfo.jsx
+++ study-plat/src/components/AboutInfo/AboutInfo.jsx
@@ -13,16 +13,14 @@ const AboutInfo = () => {
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                     when an unknown printer took a galley of type and scrambled it to make a type 
                     specimen book. It has survived not only five centuries, but also the leap 
-                    into electronic typesetting, remaining essentially unchanged. 
-                    It was popularised in the 1960s with the release of Letraset sheets 
-                    containing Lorem Ipsum passages, and more recently with desktop 
-                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    into electronic typesetting, remaining essentially unchanged.
                 </p>
 
-                <div className={styles.button_container}>
+                <div className={styles.small_container}>
                     <Button title="Go Study"/>
-                    <img src={donutImage} className={styles.picture}/>
                 </div>
+                
+                <img src={donutImage} className={styles.picture}/>
             </div>
         </div>
     )
