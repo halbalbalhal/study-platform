ac058a831bf96e2d85815fbc3de6f30343140940
Stupid_Lumpen <20miroslav06@gmail.com>
2023-06-10 15:18:12 +0300

    Header...

--- study-plat/src/components/Header/Nav/Nav.jsx
+++ study-plat/src/components/Header/Nav/Nav.jsx
@@ -3,38 +3,7 @@ import Link from '../Nav Link/Link'
 import {useEffect} from 'react'
 
 const Nav = () => {
-    const getSelected = () => {
-        const path = document.location.pathname
-        let index
-        
-        switch(path) {
-            case '/homescreen':
-                index = 0
-                break
-            case '/course':
-                index = 1
-                break
-            case '/profile':
-                index = 2
-                break
-            case '/about':
-                index = 3
-                break
-            default: 
-                index = 0    
-        }
-    
-        const selected = document.getElementById('header__nav').children[index]
-        return selected
-    }
-
-    const highlight = () => {
-        const selected = getSelected()
-        selected.classList.add(styles.highlight)
-    }
-
     useEffect(highlight)
-
     return (
         <nav className={styles.nav} id='header__nav'>
             <Link value={'Home'} path={'/homescreen'} />
@@ -43,6 +12,36 @@ const Nav = () => {
             <Link value={'About'} path={'/about'} />
         </nav>
     )
+}
+
+function getSelected() {
+    const path = document.location.pathname
+    let index
+    
+    switch(path) {
+        case '/homescreen':
+            index = 0
+            break
+        case '/course':
+            index = 1
+            break
+        case '/profile':
+            index = 2
+            break
+        case '/about':
+            index = 3
+            break
+        default: 
+            index = 0    
+    }
+
+    const selected = document.getElementById('header__nav').children[index]
+    return selected
+}
+
+function highlight() {
+    const selected = getSelected()
+    selected.classList.add(styles.highlight)
 }
 
 export default Nav
