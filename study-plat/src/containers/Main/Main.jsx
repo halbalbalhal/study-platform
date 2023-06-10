38cf05d33db0ee56cf32a041ba2a471c02e776e7
Stupid_Lumpen <20miroslav06@gmail.com>
2023-06-10 14:01:18 +0300

    Сделал адаптив для Main

--- study-plat/src/containers/Main/Main.jsx
+++ study-plat/src/containers/Main/Main.jsx
@@ -25,29 +25,32 @@ const Main = () => {
             <Header />
 
             <div className={styles.container}>
-                <div className={styles.column}>
-                    <iframe
-                        src="https://www.youtube.com/embed/uw2EdOytA-o"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowFullScreen
-                    ></iframe>
+                
+                <div className={styles.wrapper}>
+                    <div className={styles.column}>
+                        <iframe
+                            src="https://www.youtube.com/embed/uw2EdOytA-o"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowFullScreen
+                        ></iframe>
 
-                    <div className={styles.description}>
-                        <span className={styles.description__title}>{course != null && course.title}</span>
-                        <p className={styles.description__text}>{course != null && course.description}</p>
+                        <div className={styles.description}>
+                            <span className={styles.description__title}>{course != null && course.title}</span>
+                            <p className={styles.description__text}>{course != null && course.description}</p>
+                        </div>
                     </div>
-                </div>
 
-                <div className={styles.lessons}>
-                    {lessons.map((lesson) => {
-                        return (
-                            <div className={styles.lesson}>
-                                <span className={styles.lesson__title}>{lesson.title}</span>
-                                <img src={arrowImg} />
-                            </div>
-                        )
-                    })}
+                    <div className={styles.lessons}>
+                        {lessons.map((lesson) => {
+                            return (
+                                <div className={styles.lesson}>
+                                    <span className={styles.lesson__title}>{lesson.title}</span>
+                                    <img src={arrowImg} />
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </>
