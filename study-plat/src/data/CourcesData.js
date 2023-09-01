6e52bd21e15c3d43cdad2194e4e5901161fa6575
halbalbalhal <sanyabirulya@mail.ru>
2023-09-01 17:54:01 +0300

    adaptive for courses

--- study-plat/src/data/CourcesData.js
+++ study-plat/src/data/CourcesData.js
@@ -1,12 +1,15 @@
 import Rct from '../assets/images/Cources/react.png'
 import HTML from '../assets/images/Cources/html.png'
 import Python from '../assets/images/Cources/python.png'
+import Node from '../assets/images/Cources/node.png'
+import CCS from '../assets/images/Cources/css.png'
+import JS from '../assets/images/Cources/JavaScript.png'
 
 export const data =[
     {"cource_logo":Rct,"cource_name":"React", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"},
-    {"cource_logo":HTML,"cource_name":"HTML/CSS", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"},
+    {"cource_logo":HTML,"cource_name":"HTML", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"},
     {"cource_logo":Python,"cource_name":"Python", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"},
-    {"cource_logo":Python,"cource_name":"Python", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"},
-    {"cource_logo":HTML,"cource_name":"HTML/CSS", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"},
-    {"cource_logo":Rct,"cource_name":"React", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"}
+    {"cource_logo":CCS,"cource_name":"CSS", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"},
+    {"cource_logo":JS,"cource_name":"JavaScript", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"},
+    {"cource_logo":Node,"cource_name":"NodeJS", "cource_description": "Incredibly wonderful!", "cource_button": "Click me!"}
 ]
